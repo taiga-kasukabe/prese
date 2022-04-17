2769a1727db64bcfd8f9421682ccf7525757a9bc
ysato1117 <cloud241117@gmail.com>
2022-04-17 17:33:25 +0900

    use PHPMailer

--- sato/send_mail.php
+++ sato/send_mail.php
@@ -1,18 +1,44 @@
 <?php
 
-//言語設定
-mb_language("Japanese");
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+
+require('/xampp/htdocs/PHPMailer/src/PHPMailer.php');
+require('/xampp/htdocs/PHPMailer/src/Exception.php');
+require('/xampp/htdocs/PHPMailer/src/SMTP.php');
+
+mb_language("japanese");
 mb_internal_encoding("UTF-8");
 
-//メール情報
-$to = "$mail";
-$subject = "【NTT東日本】ユーザー登録が完了しました．";
-$message = "ユーザー登録が完了しました．";
-$headers = "From:yu.sato.fortest@gmail.com";
+$sendmail = new PHPMailer(true);
 
-mb_send_mail($to, $subject, $message, $headers);
+$sendmail->CharSet = "iso-2022-jp";
+$sendmail->Encoding = "7bit";
+
+try{
+
+$sendmail->isSMTP();
+$sendmail->Host = 'smtp.gmail.com';
+$sendmail->SMTPAuth = true;
+$sendmail->Username = 'yu.sato.fortest@gmail.com';
+$sendmail->Password = 'prese_ysato';
+$sendmail->SMTPSecure = 'tls';
+$sendmail->Port = 587;
+
+$sendmail->setFrom('yu.sato.fortest@gmail.com');
+$sendmail->addAddress('yu.sato.r6@dc.tohoku.ac.jp');
+
+$sendmail->Subject = mb_encode_mimeheader('【NTT東日本】登録完了');
+$sendmail->Body = mb_convert_encoding('登録が完了しました．', "JIS", "UTF-8");
+
+$sendmail->send();
+} catch (Exception $e) {
+    echo "Error:{$sendmail->ErrorInfo}";
+}
 
 //参考
 //https://miya-system-works.com/blog/detail/107
+//https://qiita.com/e__ri/items/857b12e73080019e00b5
 
 ?>
