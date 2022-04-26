d710bd13a8108685299641d4d61fb35332cc67e2
ysato1117 <cloud241117@gmail.com>
2022-04-26 14:07:36 +0900

    make home

--- sato/home.php
+++ sato/home.php
@@ -18,6 +18,7 @@ if(!$_SESSION['user']['id']){
     exit;
 }
 
+
 //データベース情報の読み込み
 include('./conf/db_conf.php');
 
@@ -28,30 +29,35 @@ try{
     $msg = $e -> getMessage();
 }
 
+$id = $_SESSION['id'];
+
 //ユーザー情報の取得
 $sql_user = "SELECT * FROM users_table WHERE id = :id";
 $stmt = $dbh->prepare($sql_user);
 $stmt->bindValue(':id', $id);
+$stmt->execute();
 $member = $stmt->fetch();
 
 //社員情報の取得
-$sql_emp = "SELECT * FROM emb_table";
+$sql_emp = "SELECT * FROM emp_table";
 $stmt = $dbh->prepare($sql_emp);
 $stmt->execute();
 $employee = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
 
-
 <body>
 <h1>ホーム</h1>
 
 <p>こんにちは、<?php echo $member['username']; ?> さん</p>
 
-<a href="./mypage.php">マイページ</a>
-<a href="./diagnose.php">簡易診断はこちら</a>
+<a href="./mypage.php">マイページ</a><br>
+<a href="./diagnose.php">簡易診断はこちら</a><br><br>
 
-<h2><?php echo $employee[0]['empname']; ?></h2>
-<p>年次：<?php echo $employee[0]['empyear']; ?></p>
-<p>役職：<?php echo $employee[0]['empjob']; ?></p>
-<p>年次：<?php echo $employee[0]['empcareer']; ?></p>
+<?php for ($num = 0; $num < count($employee); $num++) { ?>
+    <h2><?php echo $employee[$num]['empname']; ?></h2>
+    <img src="./images/<?php echo $employee[$num]['empimg_id'] ?>" width="300">
+    <p>年次：<?php echo $employee[$num]['empyear']; ?></p>
+    <p>役職：<?php echo $employee[$num]['empjob']; ?></p>
+    <p>職歴：<?php echo $employee[$num]['empcareer']; ?></p><br><br><br>
+<?php } ?>
