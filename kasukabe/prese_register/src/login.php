a5d16c18683423aa8fec8789ba9936e0a3a3972a
taiga-kasukabe <taiga.kasukabe@gmail.com>
2022-04-27 10:22:38 +0900

    change .gitignore

--- kasukabe/prese_register/src/login.php
+++ kasukabe/prese_register/src/login.php
@@ -27,7 +27,7 @@ if (!isset($member['id']) || !password_verify($_POST['pass'], $member['password'
     //save the user's data in DB on SESSION
     $_SESSION['id'] = $member['id'];
     $msg = 'ログインしました。';
-    $link = '<a href="./home_mouseover.php">ホームへ</a>';
+    $link = '<a href="./home.php">ホームへ</a>';
 }
 ?>
 
