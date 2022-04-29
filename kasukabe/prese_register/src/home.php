39a740da81f3cb0f2606153db52e49f1ef456435
taiga-kasukabe <taiga.kasukabe@gmail.com>
2022-04-30 02:50:03 +0900

    complete function of search

--- kasukabe/prese_register/src/home.php
+++ kasukabe/prese_register/src/home.php
@@ -5,8 +5,8 @@ session_start();
 
 // 変数定義
 include('../conf/db_conf.php');
-$employee = array();
-$temp = 0;
+$err_msg = array();
+
 
 try {
     $options = array(
@@ -22,24 +22,51 @@ try {
 } catch (PDOException $e) {
     $msg = $e->getMessage();
 }
-$id = $_SESSION['id'];
 
+// ユーザ情報取得
+$id = $_SESSION['id'];
 $sql = "SELECT * FROM users_table WHERE id = :id";
 $stmt = $dbh->prepare($sql);
 $stmt->bindValue(':id', $id);
 $stmt->execute();
 $member = $stmt->fetch();
 
+// 社員リスト取得
 $sql = "SELECT * FROM empDB";
 $stmt = $dbh->prepare($sql);
 $stmt->execute();
 $employee = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-$sql = "SELECT * FROM `empDB` WHERE empyear = (SELECT MAX(empyear) FROM empDB)";
+// 社員最大年次取得
+$sql = "SELECT * FROM empDB WHERE empyear = (SELECT MAX(empyear) FROM empDB)";
 $stmt = $dbh->prepare($sql);
 $stmt->execute();
 $employeeYear = $stmt->fetch();
 $empyearMax = $employeeYear['empyear'];
+
+// 社員検索
+if (!empty($_POST)) {
+    // バリデーション
+    $err_msg['flag'] = true;
+    if ($_POST['empyearB4'] > $_POST['empyearAft']) {
+        $err_msg['empyear'] = "正しい範囲を選択してください";
+        $err_msg['flag'] = false;
+    }
+    if (empty($_POST['job'])) {
+        $err_msg['empjob'] = "どれか一つ選択してください";
+        $err_msg['flag'] = false;
+    }
+    if ($err_msg['flag'] === true) {
+        $sql = "SELECT * FROM empDB WHERE emptag1=:gender AND emptag3 >= :empyearMin AND emptag3 <= :empyearMax AND emptag2 = :empjob";
+        $stmt = $dbh->prepare($sql);
+        $stmt->bindValue(':gender', $_POST['gender']);
+        $stmt->bindValue(':empyearMin', $_POST['empyearB4']);
+        $stmt->bindValue(':empyearMax', $_POST['empyearAft']);
+        $stmt->bindValue(':empjob', $_POST['job']);
+        $stmt->execute();
+        $employee = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
 ?>
 
 
@@ -51,32 +78,34 @@ $empyearMax = $employeeYear['empyear'];
 
 <h2>社員検索</h2>
 <form action="" method="POST">
-    <input type="radio" name="gender" value="woman">女性
-    <input type="radio" name="gender" value="man">男性
+    <input type="radio" name="gender" value="女性" required>女性
+    <input type="radio" name="gender" value="男性" required>男性
     <br>
-    <input type="checkbox" name="job" value="nwp">NWP
-    <input type="checkbox" name="job" value="se">SE
-    <input type="checkbox" name="job" value="serviceDev">サービス開発
+    <input type="checkbox" name="job" value="NWP">NWP
+    <input type="checkbox" name="job" value="SE">SE
+    <input type="checkbox" name="job" value="サービス開発">サービス開発
+    <span style="color:#c7243a"><?php if (!empty($err_msg['empjob'])) echo $err_msg['empjob']; ?></span>
     <br>
-    <select name="empyearB4">
+    <select name="empyearB4" required>
         <?php for ($i = 3; $i <= $empyearMax; $i++) { ?>
-            <option value="<?php echo $i; ?>year"><?php echo $i; ?></option>
+            <option value="<?php echo $i; ?>"><?php echo $i; ?></option>
         <?php } ?>
     </select>
     年目〜
-    <select name="empyearAft">
+    <select name="empyearAft" required>
         <?php for ($i = 3; $i <= $empyearMax; $i++) { ?>
-            <option value="<?php echo $i; ?>year"><?php echo $i; ?></option>
+            <option value="<?php echo $i; ?>"><?php echo $i; ?></option>
         <?php } ?>
-    </select>年目
+    </select>年目<span style="color: #c7243a;"> <?php if (!empty($err_msg['empyear'])) echo $err_msg['empyear']; ?></span>
+    <br>
     <input type="submit" value="検索">
 </form>
 
-<h2>
-    社員リスト
-</h2>
-
+<h2>社員リスト</h2>
 <div>
+    <?php if (empty($employee)) {
+        echo "該当する社員はいませんでした．";
+    } ?>
     <?php for ($n = 0; $n < count($employee); $n++) { ?>
         <div class="mouseoverParent">
             <p><?php echo $employee[$n]['empname']; ?></p>
