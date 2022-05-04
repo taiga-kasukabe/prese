34725013bf300e08a69c2a8f879ed9d6eea657f4
taiga-kasukabe <taiga.kasukabe@gmail.com>
2022-05-04 13:39:17 +0900

    simplify home.php

--- kasukabe/prese_register/src/home.php
+++ kasukabe/prese_register/src/home.php
@@ -6,6 +6,11 @@ session_start();
 // 変数定義
 include('../conf/db_conf.php');
 $err_msg = array();
+// SQL用に文字列用意
+if (!empty($_POST['job'])) {
+    $job_array = $_POST['job'];
+    $job_str = "'" . implode("', '", $job_array) . "'";
+}
 
 //DB接続
 try {
@@ -47,49 +52,22 @@ $empyearMax = $employeeYear['empyear'];
 // 社員検索
 if (!empty($_POST)) {
     // バリデーション
-    $err_msg['flag'] = true;
     if ($_POST['empyearB4'] > $_POST['empyearAft']) {
         $err_msg['empyear'] = "正しい範囲を選択してください";
-        $err_msg['flag'] = false;
     }
-    if (empty($_POST['job_nwp']) && empty($_POST['job_se']) && empty($_POST['job_srvDev'])) {
+    if (empty($_POST['job'])) {
         $err_msg['empjob'] = "どれか一つ選択してください";
-        $err_msg['flag'] = false;
     }
-    if ($err_msg['flag'] === true) { //ノーエラー時
-        $sql = "SELECT * FROM empDB WHERE emptag3 >= :empyearMin AND emptag3 <= :empyearMax ";
-        $sql_job = "AND emptag2 IN "; //職種用SQL文
+    if (empty($err_msg)) { //ノーエラー時
+        $sql = "SELECT * FROM empDB WHERE emptag3 >= :empyearMin AND emptag3 <= :empyearMax AND emptag2 IN ($job_str) ";
+        // 「両方」が選択されれば，gender用SQLなし
         if ($_POST['gender'] != "両方") {
             $sql_gender = "AND emptag1= :gender ";
             $sql .= $sql_gender;
         }
-        //選択分岐(jobを2or3選択)
-        if (!empty($_POST['job_nwp']) && !empty($_POST['job_se']) && empty($_POST['job_srvDev'])) {
-            $sql_job .= "('NWP', 'SE')";
-            $sql .= $sql_job;
-        } else if (!empty($_POST['job_nwp']) && !empty($_POST['job_se']) && empty($_POST['job_srvDev'])) {
-            $sql_job .= "('NWP', 'サービス開発')";
-            $sql .= $sql_job;
-        } else if (!empty($_POST['job_nwp']) && !empty($_POST['job_se']) && empty($_POST['job_srvDev'])) {
-            $sql_job .= "('SE', 'サービス開発')";
-            $sql .= $sql_job;
-        } else if (!empty($_POST['job_nwp']) && !empty($_POST['job_se']) && !empty($_POST['job_srvDev'])) {
-            // 何もしない
-        } else {
-            $sql_job = "AND emptag2 = :empjob";
-            $sql .= $sql_job;
-        }
         $stmt = $dbh->prepare($sql);
-        var_dump($sql);
         if ($_POST['gender'] != "両方") {
             $stmt->bindValue(':gender', $_POST['gender']);
-        }
-        if (!empty($_POST['job_nwp']) && empty($_POST['job_se']) && empty($_POST['job_nwp'])) {
-            $stmt->bindValue(':empjob', "サービス開発");
-        } else if (empty($_POST['job_nwp']) && !empty($_POST['job_se']) && empty($_POST['job_nwp'])) {
-            $stmt->bindValue(':empjob', "SE");
-        } else if (empty($_POST['job_nwp']) && empty($_POST['job_se']) && !empty($_POST['job_nwp'])) {
-            $stmt->bindValue(':empjob', "NWP");
         }
         $stmt->bindValue(':empyearMin', $_POST['empyearB4']);
         $stmt->bindValue(':empyearMax', $_POST['empyearAft']);
@@ -98,7 +76,6 @@ if (!empty($_POST)) {
     }
 }
 ?>
-
 
 <!-- ここからページ表示 -->
 <p>こんにちは，<?php echo $member['username']; ?>さん</p>
@@ -112,9 +89,9 @@ if (!empty($_POST)) {
     <input type="radio" name="gender" value="男性" required>男性
     <input type="radio" name="gender" value="両方" required>両方
     <br>
-    <input type="checkbox" name="job_nwp" value="NWP">NWP
-    <input type="checkbox" name="job_se" value="SE">SE
-    <input type="checkbox" name="job_srvDev" value="サービス開発">サービス開発
+    <input type="checkbox" name="job[]" value="NWP">NWP
+    <input type="checkbox" name="job[]" value="SE">SE
+    <input type="checkbox" name="job[]" value="サービス開発">サービス開発
     <span style="color:#c7243a"><?php if (!empty($err_msg['empjob'])) echo $err_msg['empjob']; ?></span>
     <br>
     <select name="empyearB4" required>
