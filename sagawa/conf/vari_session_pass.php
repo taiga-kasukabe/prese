a098633f2c93099c4e01803f9ad0b692a3ba76b2
TS0201 <sagawatakurou@yahoo.co.jp>
2022-05-18 15:38:37 +0900

    passreset

--- sagawa/conf/vari_session_pass.php
+++ sagawa/conf/vari_session_pass.php
@@ -1,4 +1,6 @@
 <?php
 $password = $_SESSION['user']['password'];
 $password_confirm = $_SESSION['user']['password_confirm'];
+$id = $_SESSION['user']['id'];
+//$mail = $_SESSION['user']['mail'];
 ?>
--- sagawa/reset_vali_pass.php
+++ sagawa/reset_vali_pass.php
@@ -3,7 +3,23 @@ session_start();
 $_SESSION = array();
 
 //変数定義
-include("./conf/vari_reset.php");
+//include("./conf/vari_reset.php");
+//DB接続用
+include("./conf/config.php");
+
+$password = ($_POST['password']);
+$password_confirm = ($_POST['password_confirm']);
+$err_msg = array();
+
+//データベースへ接続、テーブルがない場合は作成
+try {
+    //インスタンス化（"データベースの種類:host=接続先アドレス, dbname=データベース名,charset=文字エンコード" "ユーザー名", "パスワード", opt)
+      $pdo = new PDO(DSN, DB_USER, DB_PASS);
+    //エラー処理
+    } catch (Exception $e) {
+      echo $e->getMessage() . PHP_EOL;
+    }
+
 
 //正規表現でパスワードをバリデーション
 if (strlen($_POST['password']) < 8 || !preg_match("/^[a-zA-Z0-9]+$/", $_POST['password'])) {
@@ -18,10 +34,10 @@ if ($_POST['password'] != $_POST['password_confirm']){
 $_SESSION['err'] = array();
 $_SESSION['err'] = $_SESSION['err'] + $err_msg;
 
+$_SESSION['user'] = array();
 $_SESSION['user']['password'] = $password;
 $_SESSION['user']['password_confirm'] = $password_confirm;
-$_SESSION['user']['password_row'] = $_POST['password'];
-$_SESSION['user']['password_confirm_row'] = $_POST["password_confirm"];
+$_SESSION['user']['id'] = $_POST["id"];
 
 //条件により確認or再登録
 if(empty($_SESSION['err'])){
