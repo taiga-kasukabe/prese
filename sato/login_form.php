8dddc3d70a8830eecab5e24ad1c2c4e9b99b1aec
ysato1117 <cloud241117@gmail.com>
2022-06-05 19:00:09 +0900

    login_form css

--- sato/login_form.php
+++ sato/login_form.php
@@ -3,13 +3,13 @@
 
 <!-- ヘッダ情報 -->
 <head>
-    <!-- 文字コードをUTF-8に設定 -->
     <meta charset="UTF-8">     
-    <!-- ページのタイトルをtestに設定 -->
-    <title>ログインページ</title>
+    <title>ログイン</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="http://necolas.github.io/normalize.css">
     <link rel="stylesheet" href="./css/login_form.css">
+    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap" rel="stylesheet">    
+    <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&family=Noto+Sans+JP:wght@300&family=Shippori+Mincho&display=swap" rel="stylesheet">   
 </head>
 
 <?php
@@ -20,13 +20,13 @@ if(!empty($_SESSION['err_msg'])){
 
 <body>
 <div class="container">
-    <p>Welcome</p>
+    <p>LOGIN</p>
     <form action="login.php" method="POST">
         <div class="form">
             <input type="text" name="id" placeholder="ID" required>
             <input type="password" name="password" placeholder="Password" required>
         </div>
-        <input type="submit" value="ログイン">
+        <button type="submit" id="login_btn">Login</button>
     </form>
 </div>
 </body>
