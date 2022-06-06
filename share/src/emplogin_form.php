4e493de3b72ecaaca933b6d560fb92a03d98436d
ysato1117 <cloud241117@gmail.com>
2022-06-06 16:55:32 +0900

    emplogin css

--- share/src/emplogin_form.php
+++ share/src/emplogin_form.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html> 
+<html lang="ja"> 
+
+<!-- ヘッダ情報 -->
+<head>
+    <meta charset="UTF-8">     
+    <title>ログイン</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="http://necolas.github.io/normalize.css">
+    <link rel="stylesheet" href="./css/emplogin_form.css">
+    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap" rel="stylesheet">    
+    <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&family=Noto+Sans+JP:wght@300&family=Shippori+Mincho&display=swap" rel="stylesheet">   
+</head>
+
 <h1>社員ログインページ</h1>
 <form action="./emplogin.php" method="post">
     <div>
