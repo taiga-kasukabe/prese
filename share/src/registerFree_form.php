807bb9384b8664b3625611fb940ca630d0293585
ysato1117 <cloud241117@gmail.com>
2022-06-07 12:18:11 +0900

    layout

--- share/src/registerFree_form.php
+++ share/src/registerFree_form.php
@@ -63,74 +63,82 @@ if (!empty($employee)) {
 
 <main>
 <?php if (!empty($employee)) { ?>
+
     <h1><?php echo $employee['empname']; ?> さん</h1>
-    <h2>空き日程を登録してください</h2>
-    <!-- 表示週の変更ボタン -->
-    <?php
-    if (isset($employee)) {
-        if ($week > 0) {
-            echo '<a href="./registerFree_form.php?empid=' . $empid . '&week=' . $week - 1 . '">前の1週間</a></br>';
-        } else {
-            echo '<del>前の1週間</del></br>';
-        }
-        echo '<a href="./registerFree_form.php?empid=' . $empid . '&week=' . $week + 1 .  '">次の1週間</a>';
-    }
-    ?>
+    <div class="container">
+        <h2>空き日程の登録</h2>
 
-    <!-- 予約表 -->
-    <form action="./registerFree_confirm.php" method="GET">
-        <table>
-            <tr>
-                <th>時間(時)</th>
-                <?php for ($i = $week * 7; $i < 7 * ($week + 1); $i++)
-                    print '<th>' . date('m/d', strtotime($i . 'day')) . '(' . $weekJa[date('w', strtotime(date('Y-m-d', strtotime($i . 'day'))))] . ')</th>';
+        <div class="table">
+            <!-- 表示週の変更ボタン -->
+            <div class="move_btn">
+                <?php
+                if (isset($employee)) {
+                    if ($week > 0) {
+                        echo '<a href="./registerFree_form.php?empid=' . $empid . '&week=' . $week - 1 . '" class="prev">前の1週間</a>';
+                    } else {
+                        echo '<del class="prev">前の1週間</del>';
+                    }
+                    echo '<a href="./registerFree_form.php?empid=' . $empid . '&week=' . $week + 1 .  '" class="next">次の1週間</a>';
+                }
                 ?>
-            </tr>
-            <?php
-            for ($time = 1000; $time <= 1600; $time += 100) {
-                if ($time == 1200) {
-                    continue;
-                }
-                echo '<tr>
-                <th>' . substr_replace($time, ':', 2, 0) . '</th>';
-                for ($i =  $week * 7; $i < 7 * ($week + 1); $i++) {
-                    $cnt = 0;
-                    // 未予約日程を表示
-                    for ($j = 0; $j < count($unrsvInfo); $j++) {
-                        if ($unrsvInfo[$j]['rsvdate'] == date('Y-m-d', strtotime($i . 'day')) && date('Hi', strtotime($unrsvInfo[$j]['rsvtime'])) == $time) {
-                            print '<td>-</td>';
-                            $cnt++;
-                        }
-                    }
-                    // 予約済み日程を表示
-                    for ($k = 0; $k < count($rsvInfo); $k++) {
-                        if ($rsvInfo[$k]['rsvdate'] == date('Y-m-d', strtotime($i . 'day')) && date('Hi', strtotime($rsvInfo[$k]['rsvtime'])) == $time) {
-                            print '<td>-</td>';
-                            $cnt++;
-                        }
-                    }
-                    if ($cnt > 0) {
-                        continue;
-                    }
-                    // checkboxで実装
-                    print '<td><input type="checkbox" name="free[]" value="' . $empid . ':' .  $time . ':' . date('m/d', strtotime($i . 'day')) . ':' . date('w', strtotime(date('Y-m-d', strtotime($i . 'day')))) . '"></td>';
-                }
-                echo '</tr>';
-            }
-            ?>
-        </table>
-        <input type="submit" value="空き日程を登録">
-    </form>
-    <p>-：既に空き日程として登録済み</p>
+            </div>
 
-    <form action="./editFree_form.php" method="get">
-        <input type="hidden" name="empid" value="<?php echo $empid; ?>">
-        <input type="hidden" name="week" value="<?php echo $week; ?>">
-        <input type="submit" value="空き日程の編集へ">
-    </form>
+            <!-- 予約表 -->
+            <form action="./registerFree_confirm.php" method="GET">
+                <table>
+                    <tr>
+                        <th>時間(時)</th>
+                        <?php for ($i = $week * 7; $i < 7 * ($week + 1); $i++)
+                            print '<th>' . date('m/d', strtotime($i . 'day')) . '(' . $weekJa[date('w', strtotime(date('Y-m-d', strtotime($i . 'day'))))] . ')</th>';
+                        ?>
+                    </tr>
+                    <?php
+                    for ($time = 1000; $time <= 1600; $time += 100) {
+                        if ($time == 1200) {
+                            continue;
+                        }
+                        echo '<tr>
+                        <th>' . substr_replace($time, ':', 2, 0) . '</th>';
+                        for ($i =  $week * 7; $i < 7 * ($week + 1); $i++) {
+                            $cnt = 0;
+                            // 未予約日程を表示
+                            for ($j = 0; $j < count($unrsvInfo); $j++) {
+                                if ($unrsvInfo[$j]['rsvdate'] == date('Y-m-d', strtotime($i . 'day')) && date('Hi', strtotime($unrsvInfo[$j]['rsvtime'])) == $time) {
+                                    print '<td>-</td>';
+                                    $cnt++;
+                                }
+                            }
+                            // 予約済み日程を表示
+                            for ($k = 0; $k < count($rsvInfo); $k++) {
+                                if ($rsvInfo[$k]['rsvdate'] == date('Y-m-d', strtotime($i . 'day')) && date('Hi', strtotime($rsvInfo[$k]['rsvtime'])) == $time) {
+                                    print '<td>-</td>';
+                                    $cnt++;
+                                }
+                            }
+                            if ($cnt > 0) {
+                                continue;
+                            }
+                            // checkboxで実装
+                            print '<td><input type="checkbox" name="free[]" value="' . $empid . ':' .  $time . ':' . date('m/d', strtotime($i . 'day')) . ':' . date('w', strtotime(date('Y-m-d', strtotime($i . 'day')))) . '"></td>';
+                        }
+                        echo '</tr>';
+                    }
+                    ?>
+                </table>
+                <p>-：既に空き日程として登録済み</p>
+                <button type="submit">登録する</button>
+            </form>
+        </div>
+
+        <form action="./editFree_form.php" method="get">
+            <input type="hidden" name="empid" value="<?php echo $empid; ?>">
+            <input type="hidden" name="week" value="<?php echo $week; ?>">
+            <input type="submit" value="空き日程の編集へ">
+        </form>
+    </div>
 <?php } else { ?>
-    <h1>存在しない社員IDです</h1>
+            <h1>存在しない社員IDです</h1>
 <?php } ?>
-<input type="button" onclick="location.href='./emplogin_form.php'" value="社員変更">
+        <!-- <input type="button" onclick="location.href='./emplogin_form.php'" value="社員変更"> -->
 </main>
 </body>
