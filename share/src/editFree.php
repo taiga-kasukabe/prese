e5dda87b37f3ff5d0774db097201b4350e2ba56d
ysato1117 <cloud241117@gmail.com>
2022-06-10 12:28:44 +0900

    complete page css

--- share/src/editFree.php
+++ share/src/editFree.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html> 
+<html lang="ja"> 
+
+<!-- ヘッダ情報 -->
+<head>
+    <meta charset="UTF-8">     
+    <title>登録完了</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="http://necolas.github.io/normalize.css">
+    <link rel="stylesheet" href="../css/editFree.css">
+    <script src="https://kit.fontawesome.com/2d726a91d3.js" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&family=Noto+Sans+JP:wght@300&family=Shippori+Mincho&display=swap" rel="stylesheet">   
+</head>
+
 <?php
 session_start();
 
@@ -44,14 +58,29 @@ $stmt = $dbh->prepare($sql);
 $stmt->execute();
 ?>
 
-<h1>削除しました</h1>
-<form action="./editFree_form.php" method="get">
-    <input type="hidden" name="empid" value="<?php echo $empid[0]; ?>">
-    <input type="hidden" name="week" value="0">
-    <input type="submit" value="追加で削除">
-</form>
-<form action="./registerFree_form.php" method="GET">
-    <input type="hidden" name="empid" value="<?php echo $empid[0]; ?>">
-    <input type="hidden" name="week" value="0">
-    <input type="submit" value="空き日程を登録">
-</form>
+<body>
+<header>
+    <div class="bg">
+        <img src="../images/ntt-east_white.png" id="logo">
+    </div>
+    </script>
+</header>
+
+<main>
+<div class="container">
+    <h1>COMPLETE</h1>
+    <div class="btn">
+        <form action="./editFree_form.php" method="get">
+            <input type="hidden" name="empid" value="<?php echo $empid[0]; ?>">
+            <input type="hidden" name="week" value="0">
+            <button type="submit"  id="register">追加で削除する</button>
+        </form>
+        <form action="./registerFree_form.php" method="get">
+                <input type="hidden" name="week" value="0">
+                <input type="hidden" name="empid" value="<?php echo $empid[0]; ?>">
+                <button id="backHome" onclick="location.href='./registerFree_form.php'">ホームへ戻る(今は登録画面)</button>
+        </form>
+    </div>
+</div>
+</main>
+</body>
