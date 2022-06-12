85a370b6b65e3bb4bea7973e1f50f7cadeca81a1
taiga-kasukabe <taiga.kasukabe@gmail.com>
2022-06-12 21:32:11 +0900

    emplogin_form.phpの修正

--- share/src/emplogin.php
+++ share/src/emplogin.php
@@ -21,7 +21,7 @@ $member = $stmt->fetch();
 
 if (!isset($member['eid']) || $_POST['epass'] != $member['epassword']) {
   $msg = 'idもしくはパスワードが間違っています。';
-  $link = '<a href="./empmypage.php" class="err_msg">戻る</a>';
+  $link = '<a href="./emplogin_form.php" class="err_msg">戻る</a>';
 } else if (($_POST['epass'] = $member['epassword'])) {
   //save the user's data in DB on SESSION
   $_SESSION['eid'] = $member['eid'];
