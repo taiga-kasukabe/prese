aad114c597893524d7b8c60756981208aeb5bab1
taiga-kasukabe <taiga.kasukabe@gmail.com>
2022-06-12 20:40:09 +0900

    セッション切れの対応を追加

--- share/src/mypage.php
+++ share/src/mypage.php
@@ -27,45 +27,45 @@ try {
     $msg = $e->getMessage();
 }
 
+// SESSIONが切れてないか確認
+if (!empty($_SESSION['id'])) {
+    $id = $_SESSION['id'];
+    //users_table接続
+    // ログインしている学生のデータ取得
+    $sql_user = "SELECT * FROM users_table WHERE id = :id";
+    $stmt = $dbh->prepare($sql_user);
+    $stmt->bindValue(':id', $id);
+    $stmt->execute();
+    $member = $stmt->fetch();
 
-$id = $_SESSION['id'];
+    //rsvdb接続
+    // rsvdbのデータを全て取得
+    $sql_rsv = "SELECT * FROM rsvdb";
+    $stmt = $dbh->prepare($sql_rsv);
+    $stmt->execute();
+    $employees = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-//users_table接続
-// ログインしている学生のデータ取得
-$sql_user = "SELECT * FROM users_table WHERE id = :id";
-$stmt = $dbh->prepare($sql_user);
-$stmt->bindValue(':id', $id);
-$stmt->execute();
-$member = $stmt->fetch();
+    // rsvdbのうちログインしている学生の予約データのみ取得
+    $sql_rsv = "SELECT * FROM rsvdb WHERE stuid = '$id'";
+    $stmt = $dbh->prepare($sql_rsv);
+    $stmt->execute();
+    $stuid = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-//rsvdb接続
-// rsvdbのデータを全て取得
-$sql_rsv = "SELECT * FROM rsvdb";
-$stmt = $dbh->prepare($sql_rsv);
-$stmt->execute();
-$employees = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    //empDB接続
+    // rsvdbのうちログインしている学生が予約しているデータのempidを取得し，
+    // それと一致するempidのデータをemp_tableから取得する
+    $sql_emp = "SELECT * FROM emp_table WHERE empid in (SELECT empid FROM rsvdb WHERE stuid = '$id')";
+    $stmt = $dbh->prepare($sql_emp);
+    $stmt->execute();
+    $employee = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-// rsvdbのうちログインしている学生の予約データのみ取得
-$sql_rsv = "SELECT * FROM rsvdb WHERE stuid = '$id'";
-$stmt = $dbh->prepare($sql_rsv);
-$stmt->execute();
-$stuid = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-//empDB接続
-// rsvdbのうちログインしている学生が予約しているデータのempidを取得し，
-// それと一致するempidのデータをemp_tableから取得する
-$sql_emp = "SELECT * FROM emp_table WHERE empid in (SELECT empid FROM rsvdb WHERE stuid = '$id')";
-$stmt = $dbh->prepare($sql_emp);
-$stmt->execute();
-$employee = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-//ename
-// emp_tableの情報をすべて取得する
-$sql_emp = "SELECT * FROM emp_table";
-$stmt = $dbh->prepare($sql_emp);
-$stmt->execute();
-$ename = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
+    //ename
+    // emp_tableの情報をすべて取得する
+    $sql_emp = "SELECT * FROM emp_table";
+    $stmt = $dbh->prepare($sql_emp);
+    $stmt->execute();
+    $ename = $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
 ?>
 
 <body>
@@ -79,57 +79,65 @@ $ename = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <main>
 
         <body>
-            <div class="top">
-                <h1><?php echo $member['username']; ?> さん</h1>
-                <div class="link">
-                    <a href="./reset_pass_form.php">パスワード再登録はこちら</a>
-                    <a href="./withdrawal_form.php">退会はこちら</a>
-                </div>
-            </div>
-            <div class="rsv_list">
-                <h2>予約内容</h2>
-                <?php for ($n = 0; $n < count($stuid); $n++) { ?>
-                    <div class="rsv_content">
-                        <div class="rsv_text">
-                            <?php for ($i = 0; $i < count($ename); $i++) {
-                                if ($stuid[$n]['empid'] == $ename[$i]['empid']) { ?>
-                                    <p><span class="tag"><i class="fa-solid fa-user"></i>面談相手</span>&nbsp;&nbsp;<?php echo $ename[$i]['empname']; ?></p>
-                            <?php }
-                            } ?>
-                            <!-- ここの日程表示を自分で変える -->
-
-                            <?php $rsvtime = $stuid[$n]['rsvdate'] ?>
-                            <p><span class="tag"><i class="fa-solid fa-clock"></i>予約日時</span>&nbsp;&nbsp;<?php echo date('m/d', strtotime($stuid[$n]['rsvdate'])) . '(' . $weekJa[date('w', strtotime(date('Y-m-d', strtotime($stuid[$n]['rsvdate']))))] . ')'; ?>&nbsp;&nbsp;&nbsp;<?php echo date('H:i', strtotime($stuid[$n]['rsvtime'])) . '〜' . date('H:i', strtotime($stuid[$n]['rsvtime'] . " +1 hours")); ?></p>
-                            <div class="comment">
-                                <p class="comment_tag"><span class="tag"><i class="fa-solid fa-pen"></i>相談内容</span>&nbsp;&nbsp;</p>
-                                <p class="comment_data"><?php echo $stuid[$n]['comment']; ?></p>
-                            </div>
-                        </div>
-
-                        <?php if ($rsvtime <= date('Y-m-d', strtotime("+2day"))) : ?>
-                            <p>予約日2日前以降は予約の取り消しは出来ません。</p>
-                            <p>これ以降は直接連絡をお取りください。</p>
-                            <p>メールアドレス：hoge@hoge.com</p>
-                        <?php else : ?>
-                            <div class="delete_btn">
-                                <form action="./rsv_cancel.php" method="post" onSubmit="return check()">
-                                    <button type="submit" class="delete">取消</button>
-                                    <input type="hidden" name="empid" value="<?= $stuid[$n]['empid'] ?>">
-                                </form>
-                            </div>
-                        <?php endif; ?>
+            <?php if (!empty($_SESSION['id'])) { ?>
+                <div class="top">
+                    <h1><?php echo $member['username']; ?> さん</h1>
+                    <div class="link">
+                        <a href="./reset_pass_form.php">パスワード再登録はこちら</a>
+                        <a href="./withdrawal_form.php">退会はこちら</a>
                     </div>
-                <?php } ?>
-                <script type="text/javascript">
-                    function check() {
-                        let result = window.confirm('予約取り消しますか？');
-                        if (result) {
-                            return ture;
-                        } else {
-                            alert("取り消しをやめる");
-                            return false;
-                        }
-                    };
-                </script>
-            </div>
+                </div>
+                <div class="rsv_list">
+                    <h2>予約内容</h2>
+                    <?php for ($n = 0; $n < count($stuid); $n++) { ?>
+                        <div class="rsv_content">
+                            <div class="rsv_text">
+                                <?php for ($i = 0; $i < count($ename); $i++) {
+                                    if ($stuid[$n]['empid'] == $ename[$i]['empid']) { ?>
+                                        <p><span class="tag"><i class="fa-solid fa-user"></i>面談相手</span>&nbsp;&nbsp;<?php echo $ename[$i]['empname']; ?></p>
+                                <?php }
+                                } ?>
+                                <!-- ここの日程表示を自分で変える -->
+
+                                <?php $rsvtime = $stuid[$n]['rsvdate'] ?>
+                                <p><span class="tag"><i class="fa-solid fa-clock"></i>予約日時</span>&nbsp;&nbsp;<?php echo date('m/d', strtotime($stuid[$n]['rsvdate'])) . '(' . $weekJa[date('w', strtotime(date('Y-m-d', strtotime($stuid[$n]['rsvdate']))))] . ')'; ?>&nbsp;&nbsp;&nbsp;<?php echo date('H:i', strtotime($stuid[$n]['rsvtime'])) . '〜' . date('H:i', strtotime($stuid[$n]['rsvtime'] . " +1 hours")); ?></p>
+                                <div class="comment">
+                                    <p class="comment_tag"><span class="tag"><i class="fa-solid fa-pen"></i>相談内容</span>&nbsp;&nbsp;</p>
+                                    <p class="comment_data"><?php echo $stuid[$n]['comment']; ?></p>
+                                </div>
+                            </div>
+
+                            <?php if ($rsvtime <= date('Y-m-d', strtotime("+2day"))) : ?>
+                                <p>予約日2日前以降は予約の取り消しは出来ません。</p>
+                                <p>これ以降は直接連絡をお取りください。</p>
+                                <p>メールアドレス：hoge@hoge.com</p>
+                            <?php else : ?>
+                                <div class="delete_btn">
+                                    <form action="./rsv_cancel.php" method="post" onSubmit="return check()">
+                                        <button type="submit" class="delete">取消</button>
+                                        <input type="hidden" name="empid" value="<?= $stuid[$n]['empid'] ?>">
+                                    </form>
+                                </div>
+                            <?php endif; ?>
+                        </div>
+                    <?php } ?>
+                    <script type="text/javascript">
+                        function check() {
+                            let result = window.confirm('予約取り消しますか？');
+                            if (result) {
+                                return ture;
+                            } else {
+                                alert("取り消しをやめる");
+                                return false;
+                            }
+                        };
+                    </script>
+                </div>
+            <?php } else { ?>
+                <h2>セッションが切れました</h2>
+                <h2>ログインしてください</h2>
+                <a href="./login_form.php">ログインページへ</a>
+            <?php } ?>
         </body>
+    </main>
+</body>
