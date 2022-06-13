a2e4b31995409139ca1cb3c28abc69767847de89
ysato1117 <cloud241117@gmail.com>
2022-06-13 17:20:08 +0900

    session destroy

--- share/src/student/pass_register.php
+++ share/src/student/pass_register.php
@@ -12,49 +12,61 @@
     <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&family=Noto+Sans+JP:wght@300&family=Shippori+Mincho&display=swap" rel="stylesheet">
 </head>
 
-<?php
-session_start();
-
-//変数定義
-include("../../conf/vari_session_pass.php");
-include("../../conf/config.php");
-
-//データベースへ接続、テーブルがない場合は作成
-try {
-    $dbh = new PDO($dsn, $db_username, $db_password);
-} catch (PDOException $e) {
-    $msg = $e->getMessage();
-}
-
-$sql =  "UPDATE users_table SET password = :password ,password_confirm = :password_confirm WHERE id=:id";
-$stmt = $dbh->prepare($sql);
-$stmt->bindValue(':password', $password);
-$stmt->bindValue(':password_confirm', $password_confirm);
-$stmt->bindValue(':id', $id);
-//$params = array(':password' => $password, ':password_confirm' => $password_confirm);
-$stmt->execute();
-
-session_destroy();
-
-?>
-
 <body>
     <header>
         <div class="bg">
             <img src="../../images/ntt-east_white.png" id="logo">
+            <a href="./home.php" id="home">ホーム</a>
         </div>
         </script>
     </header>
 
-    <main>
-        <div class="top">
-            <h1>パスワード再設定</h1>
-        </div>
-        <div class="container">
-            <h2>再設定が完了しました</h2>
-            <p>こちらのリンクからログインしてください</p>
-            <a href="./login_form.php">ログインページへ</a>
-        </div>
-    </main>
+    <?php // SESSIONが切れてないか確認
+    if (!empty($_SESSION['id'])) { ?>
+    
+        <?php
+        session_start();
+
+        //変数定義
+        include("../../conf/vari_session_pass.php");
+        include("../../conf/config.php");
+
+        //データベースへ接続、テーブルがない場合は作成
+        try {
+            $dbh = new PDO($dsn, $db_username, $db_password);
+        } catch (PDOException $e) {
+            $msg = $e->getMessage();
+        }
+
+        $sql =  "UPDATE users_table SET password = :password ,password_confirm = :password_confirm WHERE id=:id";
+        $stmt = $dbh->prepare($sql);
+        $stmt->bindValue(':password', $password);
+        $stmt->bindValue(':password_confirm', $password_confirm);
+        $stmt->bindValue(':id', $id);
+        //$params = array(':password' => $password, ':password_confirm' => $password_confirm);
+        $stmt->execute();
+
+        session_destroy();
+
+        ?>
+        <main>
+            <div class="top">
+                <h1>パスワード再設定</h1>
+            </div>
+            <div class="container">
+                <h2>再設定が完了しました</h2>
+                <p class="login_link">こちらのリンクからログインしてください</p>
+                <a href="./login_form.php">ログインページへ</a>
+            </div>
+        </main>
+    <?php } else { ?>
+        <main>
+            <div class="container">
+                <p>セッションが切れました</p>
+                <p>ログインしてください</p>
+                <a href="./login_form.php" class="login">ログインページへ</a>
+            </div>
+        </main>
+    <?php } ?>
 </body>
 </html>
