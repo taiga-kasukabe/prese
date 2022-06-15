de33b1e0df48187f1eb7a4d1bdd7df7d4ce32a05
ysato1117 <cloud241117@gmail.com>
2022-06-15 16:10:42 +0900

    css

--- share/src/student/withdrawal_form.php
+++ share/src/student/withdrawal_form.php
@@ -5,11 +5,28 @@
 <head>
     <meta charset="UTF-8">
     <title>退会</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="http://necolas.github.io/normalize.css">
+    <link rel="stylesheet" href="../../css/withdrawal_form.css">
+    <script src="https://kit.fontawesome.com/2d726a91d3.js" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&family=Noto+Sans+JP:wght@300&family=Shippori+Mincho&display=swap" rel="stylesheet">
 </head>
 
-<div>
-    <h2>退会する場合にはこちらのボタンをクリック<h2>
-            <button type="button" id="btn">退会</button>
+<body>
+    <header>
+        <div class="bg">
+            <img src="../../images/ntt-east_white.png" id="logo">
+            <a href="./home.php" id="home">ホーム</a>
+        </div>
+    </header>
+
+    <main>
+        <div class="container">
+            <h2>退会する場合にはこちらのボタンをクリック</h2>
+            <div class="btn_list">
+                <button onclick='location.href="./mypage.php"' id="page_back">戻る</button>
+                <button type="button" id="btn">退会</button>
+            </div>
             <script type="text/javascript">
                 let btn = document.getElementById('btn');
                 btn.addEventListener('click', function() {
@@ -22,5 +39,7 @@
                     }
                 });
             </script>
-            <p><a href="./mypage.php">戻る</a></p>
-</div>
+        </div>
+    </main>
+    <script type="text/javascript" src="../../js/browserBack.js"></script>
+</body>
