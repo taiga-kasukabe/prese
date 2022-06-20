406ab78ac05fbb66880d5af1aa313885ada8044f
ysato1117 <cloud241117@gmail.com>
2022-06-21 02:43:39 +0900

    CSS

--- share/src/student/rsv_cancel.php
+++ share/src/student/rsv_cancel.php
@@ -4,7 +4,13 @@
 <head>
     <meta charset="UTF-8">
     <title>登録取り消し</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="http://necolas.github.io/normalize.css">
+    <link rel="stylesheet" href="../../css/rsv_cancel.css">
+    <script src="https://kit.fontawesome.com/2d726a91d3.js" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&family=Noto+Sans+JP:wght@300&family=Shippori+Mincho&display=swap" rel="stylesheet">
 </head>
+
 <?php
 session_start();
 include("../../conf/config.php");
@@ -30,11 +36,24 @@ if (!empty($_SESSION['id'])) {
 }
 ?>
 
-<?php if (!empty($_SESSION['id'])) { ?>
-    <h1>予約キャンセルしました</h1>
-    <a href="./mypage.php">マイページへ</a>
-<?php } else { ?>
-    <h1>セッションが切れました</h1>
-    <h2>再ログインしてください</h2>
-    <a href="./login_form.php">ログインページへ</a>
-<?php } ?>
+<body>
+    <header>
+        <div class="bg">
+            <img src="../../images/ntt-east_white.png" id="logo">
+            <a href="./home.php" id="home">ホーム</a>
+        </div>
+    </header>
+
+    <main>
+        <?php if (!empty($_SESSION['id'])) { ?>
+            <h1>予約キャンセルしました</h1>
+            <a href="./mypage.php">マイページへ</a>
+        <?php } else { ?>
+            <div class="container">
+                <p>セッションが切れました</p>
+                <p>再ログインしてください</p>
+                <a href="./login_form.php" class="login">ログインページへ</a>
+            </div>
+        <?php } ?>
+    </main>
+</body>
