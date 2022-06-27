db2525b1f703fec3fc00dc7e4e5ecd04e4943585
ysato1117 <cloud241117@gmail.com>
2022-06-27 15:44:12 +0900

    copyを統一

--- share/src/employee/empmypage.php
+++ share/src/employee/empmypage.php
@@ -44,9 +44,11 @@ if (!empty($_SESSION['empid'])) {
     $employee = $stmt->fetch();
 
     // 予約情報取得
-    $sql = "SELECT * FROM rsvdb WHERE empid = :empid ORDER BY rsvdate, rsvtime";
+    $sql = "SELECT * FROM rsvdb WHERE (empid = :empid AND ((rsvdate >= :today) AND (flag = 1)) OR ((rsvdate >= :2daysAfter) AND (flag = 0))) ORDER BY rsvdate, rsvtime";
     $stmt = $dbh->prepare($sql);
     $stmt->bindValue(':empid', $empid);
+    $stmt->bindValue(':today', date('Y-m-d'));
+    $stmt->bindValue(':2daysAfter', date('Y-m-d',strtotime('+2day')));
     $stmt->execute();
     $rsvInfo = $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
@@ -69,15 +71,15 @@ $stuInfo = $stmt->fetchAll(PDO::FETCH_ASSOC);
         <main>
             <h1><?php echo $employee['empname']; ?> さん</h1>
             <h2><i class="fa-solid fa-clipboard-list"></i>予約可能日程一覧</h2>
-            <table>
-                <tr>
-                    <th>日付</th>
-                    <th>時間</th>
-                    <th>予約状況</th>
-                    <th>確認</th>
-                </tr>
-                <?php for ($i = 0; $i < count($rsvInfo); $i++) {
-                    if (date('Y-m-d') <= $rsvInfo[$i]['rsvdate']) { ?>
+            <?php if (!empty($rsvInfo)) { ?>
+                <table>
+                    <tr>
+                        <th>日付</th>
+                        <th>時間</th>
+                        <th>予約状況</th>
+                        <th>確認</th>
+                    </tr>
+                    <?php for ($i = 0; $i < count($rsvInfo); $i++) { ?>
                         <tr>
                             <?php print '<td>' . date('m/d', strtotime($rsvInfo[$i]['rsvdate'])) . '(' . $weekJa[date('w', strtotime(date('Y-m-d', strtotime($rsvInfo[$i]['rsvtime']))))] . ')' . '</td><td>' . date('H:i', strtotime($rsvInfo[$i]['rsvtime'])) . '〜' . date('H:i', strtotime($rsvInfo[$i]['rsvtime'] . " +1 hours")) . '</td><td>';
                             if ($rsvInfo[$i]['flag'] == 1) {
@@ -99,42 +101,33 @@ $stuInfo = $stmt->fetchAll(PDO::FETCH_ASSOC);
                             <div class="works_modal_mask"></div>
                             <div class="works_modal_window">
                                 <div class="works_modal_content">
-                                    <p>学生情報</p>
-                                    <p>名前：
-                                        <?php for ($j = 0; $j < count($stuInfo); $j++) {
-                                            if ($rsvInfo[$i]['stuid'] == $stuInfo[$j]['id']) {
-                                                echo $stuInfo[$j]['username'];
-                                                $num = $j;
-                                            }
-                                        } ?>さん</p>
-                                    <p>大学情報：<?php echo $stuInfo[$num]['school'] . ' ' . $stuInfo[$num]['department1'] . ' ' . $stuInfo[$num]['department2'] . ' ' . $stuInfo[$num]['student_year']; ?></p>
-                                    <p>相談内容：<?php echo $rsvInfo[$i]['comment']; ?></p>
-                                    <p>メールアドレス：<span class="copy-text" data-mail="mail-<?php echo $i ?>" ><?php  echo $stuInfo[$num]['mail']; ?></span></p>
+                                    <p class="title">学生情報</p>
+                                    <div class="stuInfo">
+                                        <p><span class="tag"><i class="fa-solid fa-user"></i>面談相手</span>
+                                            <?php for ($j = 0; $j < count($stuInfo); $j++) {
+                                                if ($rsvInfo[$i]['stuid'] == $stuInfo[$j]['id']) {
+                                                    echo $stuInfo[$j]['username'];
+                                                    $num = $j;
+                                                }
+                                            } ?>さん</p>
+                                        <p><span class="tag"><i class="fa-solid fa-clock"></i>大学情報</span><?php echo $stuInfo[$num]['school'] . ' ' . $stuInfo[$num]['department1'] . ' ' . $stuInfo[$num]['department2'] . ' ' . $stuInfo[$num]['student_year']; ?></p>
+                                        <div class="comment">
+                                            <p class="comment_tag"><span class="tag"><i class="fa-solid fa-pen"></i>相談内容</span></p>
+                                            <p class="comment_data"><?php echo $rsvInfo[$i]['comment']; ?></p>
+                                        </div>
+                                        <p><span class="tag"><i class="fa-solid fa-clock"></i>メールアドレス</span><span class="copy-text" data-mail="mail-<?php echo $i ?>" ><?php  echo $stuInfo[$num]['mail']; ?></span></p>
+                                    </div>
                                     <button class="copy-btn" data-mail-copy="mail-<?php echo $i ?>">メールアドレスをコピー</button>
-                                    
-                                    <script>
-                                        let copy_text = document.querySelector('#copy-text').textContent;
-                                        let copy_btn = document.querySelector('#copy-btn');
-
-                                        copy_btn.addEventListener(`click`, () => {
-                                            navigator.clipboard.writeText(copy_text).then(() => {
-                                                // true
-                                                alert("コピーしました！ : ");
-                                            }, () => {
-                                                // false
-                                                alert("コピーできていません : ");
-                                            });
-                                        });
-                                    </script>
                                 </div>
                                 <div class="works_modal_close">✖</div>
                             </div>
                         </div>
                         <!-- モーダルウインドウここまで -->
-
-                <?php }
-                } ?>
-            </table>
+                    <?php } ?>
+                </table>
+            <?php } else { ?>
+                <p class="noRsvComment">現在登録している予約可能日程はありません</p>
+            <?php } ?>
 
             <div class="btn_list">
                 <form action="./registerFree_form.php" method="get">
@@ -146,7 +139,9 @@ $stuInfo = $stmt->fetchAll(PDO::FETCH_ASSOC);
                     <button type="submit" class="schedule">削除</button>
                 </form>
             </div>
-            <a href="./emplogout.php">ログアウト</a>
+            <script>
+                
+            </script>
         </main>
     <?php } else { ?>
         <main>
