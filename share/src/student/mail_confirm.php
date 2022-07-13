0efcda5996f66739d5ef715c7006c9ed375a82a9
taiga-kasukabe <taiga.kasukabe@gmail.com>
2022-07-13 11:34:53 +0900

    cssリンクの調整

--- share/src/student/mail_confirm.php
+++ share/src/student/mail_confirm.php
@@ -9,7 +9,7 @@
     <!-- ページのタイトルをtestに設定 -->
     <title>登録完了</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="http://necolas.github.io/normalize.css">
+    <link rel="stylesheet" href="//necolas.github.io/normalize.css">
     <link rel="stylesheet" href="../../css/mail_confirm.css">
     <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&family=Noto+Sans+JP:wght@300&family=Shippori+Mincho&display=swap" rel="stylesheet">
 </head>
