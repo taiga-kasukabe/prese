898348ec9bb0f34d1a944efb364ad951be4ec592
taiga-kasukabe <taiga.kasukabe@gmail.com>
2022-07-13 22:03:19 +0900

    soft coding at mail_send_register.php

--- share/src/student/mail_send_register.php
+++ share/src/student/mail_send_register.php
@@ -21,14 +21,14 @@ $mail = new PHPMailer(true);
 
 try {
     //ホスト（さくらのレンタルサーバの初期ドメイン）
-    // $host = $mail_address;
-    $host = 'ntteast.sakura.ne.jp';
+    $host = $mail_host;
+    // $host = 'ntteast.sakura.ne.jp';
 
     //メールアカウントの情報（さくらのレンタルサーバで作成したメールアカウント）
-    // $user = $mail_address;
-    // $password = $mail_pass;
-    $user = 'ntteast_prese@ntteast.sakura.ne.jp';
-    $password = 'Webeast2022';
+    $user = $mail_address;
+    $password = $mail_pass;
+    // $user = 'ntteast_prese@ntteast.sakura.ne.jp';
+    // $password = 'Webeast2022';
 
     //差出人
     // $from = $mail_address;
