fafb88d7710e0d74fa00b1449c3168de0eaab3f2
ysato1117 <cloud241117@gmail.com>
2022-07-20 15:38:48 +0900

    内々定側のログインなしパスワード変更

---
+++ share/src/employee/reset_email.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>パスワード再設定</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="http://necolas.github.io/normalize.css">
+    <link rel="stylesheet" href="../../css/employee/reset_email.css">
+    <script src="https://kit.fontawesome.com/2d726a91d3.js" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&family=Noto+Sans+JP:wght@300&family=Shippori+Mincho&display=swap" rel="stylesheet">
+</head>
+
+<?php
+session_start();
+//データベース情報の読み込み
+include('../../conf/config.php');
+
+//データベース接続
+try{
+    $dbh = new PDO($dsn, $db_username, $db_password);
+} catch (PDOException $e) {
+    $msg = $e -> getMessage();
+}
+?>
+<body>
+    <header>
+        <div class="bg">
+            <img src="../../images/ntt-east_white.png" id="logo">
+        </div>
+    </header>
+
+    <main>
+        <div class="top">
+            <h1>パスワード再設定</h1>
+        </div>
+        <div class="container">
+            <form action="./reset_vali.php" method="post" class="form_log"> 
+                <div class="err_msg">
+                    <?php
+                    // エラーメッセージが空じゃないの時（バリデーションチェックが問題なかった時）以下の処理を行う
+                    if (!empty($_SESSION['err'])) {
+                        foreach ($_SESSION['err'] as $value) {
+                            echo $value . "<br>"; //hタグ内の改行はbr
+                        }
+                    }
+                    ?>
+                </div>
+                <div>
+                    <label>ログインID</label>
+                    <input type="text" name="id" required>
+                </div>
+                <div>
+                    <label>新しいパスワード</label>
+                    <input type="password" name="password" placeholder="半角英数字8文字以上" required>
+                </div>
+                <div>
+                    <label>新しいパスワード（再入力）</label>
+                    <input type="password" name="password_confirm" placeholder="半角英数字8文字以上" required>
+                </div>
+                <button type="submit">登録</button>
+            </form>
+        </div>
+    </main>
+</body>
---
+++ share/src/employee/reset_vali.php
@@ -0,0 +1,56 @@
+<?php
+session_start();
+// $_SESSION = array();
+
+//変数定義
+//include("./conf/vari_reset.php");
+//DB接続用
+include("../../conf/config.php");
+
+$id = $_POST['id'];
+$password = ($_POST['password']);
+$password_confirm = ($_POST['password_confirm']);
+$err_msg = array();
+
+//データベースへ接続、テーブルがない場合は作成
+try{
+    $dbh = new PDO($dsn, $db_username, $db_password);
+} catch (PDOException $e) {
+    $msg = $e -> getMessage();
+}
+
+// IDが入力されたメールアドレスのIDと一致するかチェック
+if($_POST['id'] != $_SESSION['empid']) {
+    $err_msg['id_confirm'] = '・IDが間違っています';
+}
+
+
+//正規表現でパスワードをバリデーション
+if (strlen($_POST['password']) < 8 || !preg_match("/^[a-zA-Z0-9]+$/", $_POST['password'])) {
+    $err_msg['pass_length'] = '・パスワードは8文字以上の半角英数字を入力してください';
+}
+
+//パスワード再入力のチェック
+if ($_POST['password'] != $_POST['password_confirm']){
+    $err_msg['pass_confirm'] = '・パスワード（再入力）が一致しません';
+}
+
+$_SESSION['err'] = array();
+$_SESSION['err'] = $_SESSION['err'] + $err_msg;
+
+$_SESSION['emp'] = array();
+$_SESSION['emp']['password'] = $password;
+$_SESSION['emp']['password_confirm'] = $password_confirm;
+$_SESSION['emp']['empid'] = $_POST["id"];
+
+//条件により確認or再登録
+if(empty($_SESSION['err'])){
+    // 登録
+    header('Location: ./pass_register.php');
+} else {
+    // パスワード再登録やり直し
+    header('Location: ./reset_email.php');
+
+}
+
+?>
